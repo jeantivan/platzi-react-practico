5d576c3f57bce6559f514954ccbb91a42410187d
Jean Paul Tivan <jptivan53@gmail.com>
2022-08-05 15:58:20 -0400

    Clase 9: React.useState

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "@styles/ProductItem.scss";
 
 import addToCart from "@assets/icons/bt_add_to_cart.svg";
 
 function ProductItem() {
+  const [cart, setCart] = useState("");
+
+  const handleClick = () => {
+    setCart("Hola Mundo!");
+  };
+
   return (
     <div className="ProductItem">
       <img
@@ -15,9 +21,10 @@ function ProductItem() {
           <p>$120,00</p>
           <p>Bike</p>
         </div>
-        <figure>
+        <figure onClick={handleClick}>
           <img src={addToCart} alt="" />
         </figure>
+        {cart}
       </div>
     </div>
   );
